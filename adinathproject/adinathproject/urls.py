5188a3a7fab383ccfcb68f626717aeaa5d6d3b8c
Aravind Swaminathan <71716685+Aravind2203@users.noreply.github.com>
2020-12-30 11:49:58 +0530

    Add files via upload

---
+++ adinathproject/adinathproject/urls.py
@@ -0,0 +1,38 @@
+"""adinathproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from  django.conf.urls.static import static
+from django.conf import settings
+from mainapp.views import home,add,addtobase,detail
+from mainapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('add/',add,name='add'),
+    path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/item_clear/<int:id>/', views.item_clear, name='item_clear'),
+    path('cart/item_increment/<int:id>/',
+         views.item_increment, name='item_increment'),
+    path('cart/item_decrement/<int:id>/',
+         views.item_decrement, name='item_decrement'),
+    path('cart/cart_clear/', views.cart_clear, name='cart_clear'),
+    path('cart/cart-detail/',views.cart_detail,name='cart_detail'),
+    path('hf/<category>/',views.hf,name='hf'),
+    path('addtodatabase/',addtobase,name="addtodatabase"),
+    path('detail/<int:id>',detail,name="detail")
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
